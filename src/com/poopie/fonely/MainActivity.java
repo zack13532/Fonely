30f30602c7464b88855c6c8dac51577d47cafb8c
Zachary Siegel <zack13532@gmail.com>
2014-09-27 14:19:17 -0400

    Got rid of fragment and made buttons.

--- src/com/poopie/fonely/MainActivity.java
+++ src/com/poopie/fonely/MainActivity.java
@@ -18,12 +18,18 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
+	public void recordButtonPressed(View view)
+	{
+		
+	}
+	
+	public void playbackButtonPressed(View view)
+	{
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -44,21 +50,5 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
 
 }
